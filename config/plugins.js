a08baab40e60a594664a8b94d88c081a9b45e417
Prateek Kumar <prateikjaiswal@live.com>
2025-05-02 17:55:21 +0530

    Add nodemailer email provider and enable email configuration

--- config/plugins.js
+++ config/plugins.js
@@ -13,22 +13,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  // email: {
-  //   config: {
-  //     provider: 'nodemailer',
-  //     providerOptions: {
-  //       host: env('SMTP_HOST', 'smtp.example.com'), // Default SMTP host
-  //       port: env.int('SMTP_PORT', 587),          // Default SMTP port
-  //       auth: {
-  //         user: env('SMTP_USERNAME'),
-  //         pass: env('SMTP_PASSWORD'),
-  //       },
-  //       secure: env.bool('SMTP_SECURE', false), // Use TLS by default
-  //     },
-  //     settings: {
-  //       defaultFrom: env('EMAIL_DEFAULT_FROM', 'hello@example.com'),
-  //       defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'hello@example.com'),
-  //     },
-  //   },
-  // },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'), // Default SMTP host
+        port: env.int('SMTP_PORT', 587),          // Default SMTP port
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: env.bool('SMTP_SECURE', false), // Use TLS by default
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM', 'hello@example.com'),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'hello@example.com'),
+      },
+    },
+  },
 });
