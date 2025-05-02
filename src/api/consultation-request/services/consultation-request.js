e4ffc8a1a3cfcfdb8f464a17d0ab3ba542db5a9d
Prateek Kumar <prateikjaiswal@live.com>
2025-05-02 21:00:08 +0530

    Add core service functionality to consultation-request module

--- src/api/consultation-request/services/consultation-request.js
+++ src/api/consultation-request/services/consultation-request.js
@@ -4,4 +4,15 @@
  * consultation-request service
  */
 
-module.exports = () => ({});
+const { createCoreService } = require('@strapi/strapi').factories;
+
+// Use createCoreService to extend the default service functionality
+module.exports = createCoreService('api::consultation-request.consultation-request', ({ strapi }) => ({
+  // You can add custom service methods here if needed in the future
+  // For example:
+  // async myCustomLogic(args) {
+  //   console.log('Running custom service logic...');
+  //   const result = await super.find(args); // Call default methods using 'super'
+  //   return result;
+  // }
+}));
